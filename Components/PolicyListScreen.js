fa205cc6ad42fd0438f00fa21f33c984b51cf34e
ysh12274856@gmail.com <ysh12274856@gmail.com>
2022-10-11 21:46:05 +0900

    policyList

---
+++ Components/PolicyListScreen.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect }from 'react'
+import {policyTestData} from '../testData'
+import PolicyComponet from './PolicyListCompoent'
+import { View, Text , StyleSheet , SafeAreaView, FlatList, Platform} from 'react-native'
+
+export default function PolicyListScreen() {
+  const [policyData, setPolicyData] = useState([])
+
+  useEffect(() => {
+    getPolicyData()
+  }, [])
+  
+  function getPolicyData () {
+    setPolicyData(policyTestData["data"]["empsInfo"]["emp"])
+  }
+
+  return (
+    <SafeAreaView style={styles.view}>
+      {policyData.length > 0 ? (
+          <FlatList
+            keyExtractor={(item) => item["bizId"]["_text"]}
+            data={policyData}
+            renderItem={PolicyComponet}/>
+        ) : (
+        <View style={styles.notFound}>
+          <Text>정보가 없습니다.</Text>
+        </View>
+      )}
+    </ SafeAreaView>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  view : {
+    flex : 1,
+    paddingHorizontal : 10,
+    marginHorizontal : Platform.OS == 'ios'? 10: 0
+  },
+  notFound : {
+    flex : 1,
+    alignItems : "center",
+    justifyContent : 'center'
+  }
+})
